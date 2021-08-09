ef60065f9206e19e873eeb20d26a1b3cb08b43eb
windwithlife <379163259@qq.com>
2021-08-09 10:14:40 +0800

    k3s

--- solution/k3s/install.sh
+++ solution/k3s/install.sh
@@ -1,4 +1,8 @@
 #/bin/bash
-./k3s-uninstall.sh 
-./k3s-agent-uninstall.sh
+/usr/local/bin/k3s-uninstall.sh
+/usr/local/bin/k3s-agent-uninstall.sh
 curl -sfL http://rancher-mirror.cnrancher.com/k3s/k3s-install.sh | INSTALL_K3S_MIRROR=cn sh -
+
+
+//安装helm push 
+helm plugin install https://github.com/chartmuseum/helm-pus
