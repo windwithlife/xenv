614ce05f5ba31241e0b28424e1dc2c1eb40bc645
windwithlife <379163259@qq.com>
2021-08-09 12:35:15 +0800

    k3s support helm

--- solution/k3s/install-offline.sh
+++ solution/k3s/install-offline.sh
@@ -4,9 +4,5 @@
 curl -sfL http://rancher-mirror.cnrancher.com/k3s/k3s-install.sh | INSTALL_K3S_MIRROR=cn sh -
 
 
-wget https://get.helm.sh/helm-v3.2.0-linux-amd64.tar.gz
-tar -zxvf helm-v3.2.0-linux-amd64.tar.gz
-cp linux-amd64/helm /usr/local/bin
-
 //安装helm push 
 helm plugin install https://github.com/chartmuseum/helm-pus
