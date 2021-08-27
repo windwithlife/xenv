6071ca873864f6617ea104ca009021094a0066e9
windwithlife <379163259@qq.com>
2021-08-27 23:47:10 +0800

    for initialize

--- solution/k3s/install-server.sh
+++ solution/k3s/install-server.sh
@@ -22,7 +22,7 @@ helm plugin install https://github.com/chartmuseum/helm-pus
 //安装证书管理器
 wget https://github.com/jetstack/cert-manager/releases/download/v1.0.3/cert-manager.yaml
 kubectl apply -f ./cert-manager.yaml
-//配置证书 provider
+//配置证书 provider---clusterissuer
 kubectl apply -f ./cert-issuecluster-traefik.yaml
 //申请一个lets encrypt 证书
 kubectl apply -f ./default-cert.request.yaml 
