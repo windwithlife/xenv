0553a0a5707254c859e45c6dd5a2c35c1e632759
windwithlife <379163259@qq.com>
2021-08-31 09:41:04 +0800

    update k3s install

--- solution/k3s/install-server.sh
+++ solution/k3s/install-server.sh
@@ -16,8 +16,7 @@ curl -sfL http://rancher-mirror.cnrancher.com/k3s/k3s-install.sh | INSTALL_K3S_M
 #//配置k3s可操作
 export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
 
-#安装helm push 
-helm plugin install https://github.com/chartmuseum/helm-pus
+
 
 #安装证书管理器
 wget https://github.com/jetstack/cert-manager/releases/download/v1.0.3/cert-manager.yaml
@@ -29,5 +28,8 @@ kubectl apply -f ./cert-issuecluster-traefik.yaml
 
 #临时打开traefik 的 Dashboard
  #kubectl port-forward deploy/traefik 9000:9000 --address='0.0.0.0' -n kube-system
+
+ #安装helm push 
+helm plugin install https://github.com/chartmuseum/helm-pus
 
 
