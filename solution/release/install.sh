f60b6e9da75f65f9eab43db81686b143f0ce0672
windwithlife <zhangyq@trip.com>
2022-01-18 14:39:52 +0800

    support relase with docker and docker-compose

--- solution/release/install.sh
+++ solution/release/install.sh
@@ -1,6 +1,15 @@
 #/bin/bash
 
-curl -L https://github.com/docker/compose/releases/download/1.25.4/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+#curl -L https://github.com/docker/compose/releases/download/1.25.4/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+
+#yum remove docker  docker-common docker-selinux docker-engine
+
+yum install -y yum-utils
+yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
+yum list docker-ce --showduplicates | sort -r
+yum install -y docker-ce docker-ce-cli containerd.io
+systemctl start docker
+
 
 curl -L https://github.com/docker/compose/releases/download/v2.2.2/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
 
